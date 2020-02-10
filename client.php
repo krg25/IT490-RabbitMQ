37dac65cf4fabeba39fb84f8716adbb809f4c1a0
Kenneth <krg25@njit.edu>
2020-02-10 17:30:34 -0500

    changed how messages behave

--- client.php
+++ client.php
@@ -5,19 +5,23 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("producer.ini","database");
-if (isset($argv[1]))
+if (isset($argv[1]) && isset($argv[2]) && isset($arv[3]))
 {
-  $msg = $argv[1];
+  $usr = $argv[1];
+  $pas = $argv[2];
+  $msg = $argv[3];
 }
 else
 {
+  $usr = "test";
+  $pas = "test";
   $msg = "test message";
 }
 
 $request = array();
 $request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
+$request['username'] = $usr;
+$request['password'] = $pas;
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
