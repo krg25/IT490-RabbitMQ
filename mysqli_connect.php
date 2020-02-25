9f16f45da8c4ff4909ba0af0d58eb0fe24502efb
Tyler Kazar <tk322@njit.edu>
2020-02-25 15:30:04 -0500

    implemented new error log into mysqli_connect

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,4 +1,11 @@
+#!/usr/bin/php
 <?php 
-$dbc = @mysqli_connect('localhost', 'connectionuser', 'conn3ctpass', 'StockApp') OR die ('Could not connect: '.mysqli_connect_error());
+require_once('ErrorLogger.php.inc');
+
+$log = new ErrorLogger('mysqli_connect','rabbit.ini');
+
+
+$dbc = @mysqli_connect('localhost', 'connectionuser1', 'conn3ctpass', 'StockApp') OR die ($log->logError("Connection Failure","ERROR","Failed to connect: ".mysqli_connect_error()));
+
 mysqli_set_charset($dbc, 'utf8');
 ?>
